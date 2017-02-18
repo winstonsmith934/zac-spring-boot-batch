dfc54b6ec44d43ef6d07ddd5f0a5354adfcd48de
zac <zgithub@hotmail.com>
2017-02-19 01:09:35 +0800

    writer

---
+++ src/main/java/com/example/batch/staff/StaffItemWriter.java
@@ -0,0 +1,28 @@
+package com.example.batch.staff;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.entity.TransactionStaff;
+
+@Component
+public class StaffItemWriter implements ItemWriter<TransactionStaff> {
+    private static final Logger log = LoggerFactory.getLogger(StaffItemWriter.class);
+
+    @Autowired
+    private DataSource dataSource;
+    
+	@Override
+	public void write(List<? extends TransactionStaff> items) throws Exception {
+		for (int i =0; i < items.size(); i++){
+			log.debug(items.get(i).getStaffName());
+		}
+	}
+}
